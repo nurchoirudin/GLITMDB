0d3b3e231a403d3412f9a6ac6a65e41cfea7af16
Nur Choirudin <nur.choirudin@sehatq.com>
2023-02-15 16:35:43 +0700

    remove gitignore

---
+++ Pods/Target Support Files/Pods-GLITMDB-GLITMDBUITests/Pods-GLITMDB-GLITMDBUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GLITMDB_GLITMDBUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GLITMDB_GLITMDBUITests
+@end
---
+++ Pods/Target Support Files/Pods-GLITMDB/Pods-GLITMDB-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GLITMDB : NSObject
+@end
+@implementation PodsDummy_Pods_GLITMDB
+@end
